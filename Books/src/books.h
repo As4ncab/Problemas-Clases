49bf68ae59f27ed3c4214fc69621e04681001284
As4ncab <alu0101482323@ull.edu.es>
2023-07-04 11:34:01 +0000

    Añadido .h

---
+++ Books/src/books.h
@@ -0,0 +1,62 @@
+/**
+ * @file books.h
+ * @author Alexia Sánchez Cabrera (alu0101482323@ull.edu.es)
+ *
+ * @brief Escriba un programa books.cc que defina una clase Book. Esta clase
+ *        debe permitir almacenar las características de un libro: título, año
+ *        de publicación y precio. También debe poseer un método que permita
+ *        calcular el precio del libro con impuestos.
+ *
+ *        Una vez definida la clase, implemente una función principal donde
+ *        instancie varios libros. Muestre el precio de todos los libros creados
+ *        considerando un porcentaje de impuestos pasados desde la línea de
+ *        comandos.
+ *
+ * @version 0.1
+ * @date 2023-07-04
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+class Books {
+ public:
+  // Constructores
+  Books() {}
+  Books(std::string, int, double);
+  // Destructor
+  ~Books() {}
+
+  // Getters & Setters
+  std::string GetTitle(void) const;
+  int GetYear(void) const;
+  double GetPrice(void) const;
+  double GetTaxedPrice(void) const;
+
+  void SetTitle(const std::string&);
+  void SetYear(const int&);
+  void SetPrice(const double&);
+  void SetTaxedPrice(const double&);
+
+  // Métodos
+  double TaxedPrice(const double&);
+
+  // Sobrecarga de Operadores E/S
+  friend std::ostream& operator<<(std::ostream&, Books&);
+
+ private:
+  // Atributos privados de la clase Books
+  std::string title_{" - "};
+  int year_of_publish_{0};
+  double price_{0.0};
+  double taxed_price_{price_};
+};
+
+// Funciones de Información
+bool CheckCorrectParams(int, char*, const int);
+void Usage(char*);
+void ProgramPurpose(void);
