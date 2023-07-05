d0ab27f09ec1b8cb8540829f1c4fa5bfd9dec5e3
As4ncab <alu0101482323@ull.edu.es>
2023-07-05 11:07:44 +0000

    Declaración main()

--- Timer/test/tests_main.cc
+++ Timer/test/tests_main.cc
@@ -17,4 +17,14 @@
  *
  */
 
+#include <iostream>
+
 #include "gtest/gtest.h"
+
+GTEST_API_ int main(int argc, char* argv[]) {
+  std::cout << "Ejecutando main() del programa tests_main.cc\n" << std::endl;
+
+  testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
+}
