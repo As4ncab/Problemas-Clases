00287de973e884018829e673e419af78e69ee0b4
As4ncab <alu0101482323@ull.edu.es>
2024-01-03 12:00:58 +0000

    Minor changes

--- Books/test/books_test.cc
+++ Books/test/books_test.cc
@@ -18,49 +18,47 @@
 namespace {
 Books test_book{"a", 23, 21.0};
 
-// Método TaxedPrice()
+///< Method TaxedPrice()
 TEST(TaxedPrice, TaxedPriceDifferentToPrice) {
-  // Impuesto del 7%
+  ///< 7% tax
   test_book.TaxedPrice(7);
   EXPECT_EQ(test_book.GetTaxedPrice(), 22.47);
-
-  // Impuesto del 15%
+  ///< 15% tax
   test_book.TaxedPrice(15);
   EXPECT_EQ(test_book.GetTaxedPrice(), 24.15);
 }
 
 TEST(TaxedPrice, TaxEqualToZero) {
-  // Impuesto del 0% (precio se mantiene)
+  ///< 0% tax (price unchanged)
   test_book.TaxedPrice(0);
   EXPECT_EQ(test_book.GetTaxedPrice(), test_book.GetPrice());
 }
 
-// Función CheckCorrectParams()
+///< Function CheckCorrectParams()
 TEST(CorrectParams, CorrectAmountOfParameters) {
-  // argc == 2 parámetros
+  ///< argc == 2 parameters
   EXPECT_EQ(CheckCorrectParams(2, 2), true);
 }
 
 TEST(CorrectParams, IncorrectAmountOfParameters) {
-  // argc < 2 parámetros
+  ///< argc < 2 parameters
   EXPECT_EQ(CheckCorrectParams(1, 2), 0);
-
-  // argc > 2 parámetros
+  ///< argc > 2 parameters
   EXPECT_EQ(CheckCorrectParams(3, 2), 0);
 }
 
-// Función ToIntegrer
-TEST(ToInt, ConverToIntegrer) {
-  EXPECT_EQ(ToIntegrer("7"), 7);
-  EXPECT_EQ(ToIntegrer("15"), 15);
-  EXPECT_EQ(ToIntegrer("512"), 512);
-  EXPECT_EQ(ToIntegrer("1000000"), 1000000);
-}
+// ///< Function ToIntegrer()
+// TEST(ToInt, ConverToIntegrer) {
+//   EXPECT_EQ(ToIntegrer("7"), 7);
+//   EXPECT_EQ(ToIntegrer("15"), 15);
+//   EXPECT_EQ(ToIntegrer("512"), 512);
+//   EXPECT_EQ(ToIntegrer("1000000"), 1000000);
+// }
 
-TEST(ToInt, ConvertToIntegererWithLetters) {
-  EXPECT_EQ(ToIntegrer("7aaaaaaaaaaaaa"), 7);
-  EXPECT_EQ(ToIntegrer("70eeee3rrr0"), 7030);
-  EXPECT_EQ(ToIntegrer(""), 0);
-  EXPECT_EQ(ToIntegrer("745fdsjgvnj@#~$%$&4342"), 7454342);
-}
-}  // namespace
+// TEST(ToInt, ConvertToIntegererWithLetters) {
+//   EXPECT_EQ(ToIntegrer("7aaaaaaaaaaaaa"), 7);
+//   EXPECT_EQ(ToIntegrer("70eeee3rrr0"), 7030);
+//   EXPECT_EQ(ToIntegrer(""), 0);
+//   EXPECT_EQ(ToIntegrer("745fdsjgvnj@#~$%$&4342"), 7454342);
+// }
+}  ///< namespace
--- Books/test/tests_main.cc
+++ Books/test/tests_main.cc
@@ -17,8 +17,6 @@
 
 GTEST_API_ int main(int argc, char* argv[]) {
   std::cout << "Ejecutando main() del programa tests_main.cc\n" << std::endl;
-
   testing::InitGoogleTest(&argc, argv);
-
   return RUN_ALL_TESTS();
 }
